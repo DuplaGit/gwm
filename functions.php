9e674f7a2cee169725e5506c291780a6cef6eaf8
ldelriof <dupla@MacBook-de-Luis.local>
2014-08-20 13:35:00 -0500

    prueba funcs

--- functions.php
+++ functions.php
@@ -62,7 +62,8 @@ function get_excerpt($count){
 function get_excerpt_h1(){
 	global $post;
     $content = $post->post_content;//get_the_content();
-  	print_r($post);
+    // version luis
+  	// print_r($post);
 	 // $content = preg_replace('/\[([^_]+)\]/', '', $content);
 	 // $content = apply_filters('the_content', $content);
 	// $content = str_replace(']]>', ']]>', $content);
